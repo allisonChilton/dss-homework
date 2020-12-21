0f2e55aafc9c40c9e90cdad8f2cb9d2beab9207e
Allison Chilton <allison.chilton@protonmail.com>
2020-12-21 00:06:29 -0700

    rectangle drawing with textures

--- src/graphics_layers.rs
+++ src/graphics_layers.rs
@@ -1,5 +1,7 @@
 extern crate image;
+extern crate cgmath;
 
+use cgmath::{Matrix4, Vector2};
 use glium::glutin;
 use glium::Surface;
 use glium::texture::Texture2d;
@@ -18,25 +20,47 @@ struct Vertex {
 
 #[derive(Copy, Clone)]
 struct Rectangle {
-    corners: [f32; 4],
+    center: [f32; 2],
+    size: [f32; 2],
     texture_key: &'static str,
 }
 
 impl Rectangle{
 
-    fn draw(self, frame: &mut glium::Frame, display: &glium::Display, program: &glium::Program, tex_cache: &HashMap<&str, Texture2d>){
-        let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
-        let v0 = Vertex{ position: [self.corners[0], self.corners[1]]};
-        let v1 = Vertex{ position: [self.corners[2], self.corners[1]]};
-        let v2 = Vertex{ position: [self.corners[2], self.corners[3]]};
-        let v3 = Vertex{ position: [self.corners[0], self.corners[3]]};
-        let shape = vec![v0, v1, v3];
-        let vb = glium::VertexBuffer::new(display, &shape).unwrap();
+    fn draw(self, frame: &mut glium::Frame, indices: &glium::IndexBuffer<u16>, display: &glium::Display,
+            program: &glium::Program, tex_cache: &HashMap<&str, Texture2d>, perspective: [[f32; 4]; 4]){
 
-        frame.draw(&vb, &indices, &program, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
-        let shape = vec![v2, v1, v3];
-        let vb = glium::VertexBuffer::new(display, &shape).unwrap();
-        frame.draw(&vb, &indices, &program, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
+        let vb = glium::VertexBuffer::empty_dynamic(display, 4).unwrap();
+        let rect_size = Vector2 {
+            x: self.size[0],
+            y: self.size[1],
+        };
+
+        let mut rect_position = Vector2 {
+            x: self.center[0],
+            y: self.center[1],
+        };
+
+        let left = rect_position.x - rect_size.x / 2.0;
+        let right = rect_position.x + rect_size.x / 2.0;
+        let top = rect_position.y + rect_size.y / 2.0;
+        let bottom = rect_position.y - rect_size.y / 2.0;
+
+
+        let v0 = Vertex{ position: [left, top]};
+        let v1 = Vertex{ position: [right, top]};
+        let v2 = Vertex{ position: [left, bottom]};
+        let v3 = Vertex{ position: [right, bottom]};
+        let shape = vec![v0, v1, v2, v3];
+        vb.write(&shape);
+
+        let texture = tex_cache.get(self.texture_key).unwrap();
+
+        let uniforms = uniform!{
+            projection: perspective,
+            tex: texture
+        };
+        frame.draw(&vb, indices, program, &uniforms, &Default::default()).unwrap();
 
     }
 }
@@ -62,21 +86,66 @@ fn keyboard_input(event: &glutin::event::WindowEvent<'_>, debounce: &mut bool) -
         None => return (0.0, 0.0),
     };
     match key {
-        glutin::event::VirtualKeyCode::Up => (0.0,-0.1),
-        glutin::event::VirtualKeyCode::Down => (0.0, 0.1),
-        glutin::event::VirtualKeyCode::Left => (-0.1, 0.0),
-        glutin::event::VirtualKeyCode::Right => (0.1, 0.0),
+        glutin::event::VirtualKeyCode::Up => (0.0,-10.0),
+        glutin::event::VirtualKeyCode::Down => (0.0, 10.0),
+        glutin::event::VirtualKeyCode::Left => (-10.0, 0.0),
+        glutin::event::VirtualKeyCode::Right => (10.0, 0.0),
         glutin::event::VirtualKeyCode::Return => (1.0, 1.0),
         _ => (0.0, 0.0),
     }
 }
 
+const SCREEN_WIDTH: u32 = 1024;
+const SCREEN_HEIGHT: u32 = 768;
+
+
+const VERTEX_SHADER_SRC: &str = r#"
+     #version 140
+    // Input parameter from the Vertex struct.
+    in vec2 position;
+    // Uniform parameter passed in from the frame.draw() call.
+    uniform mat4 projection;
+    // Output texture coordinates that gets passed into the fragment shader.
+    out vec2 v_tex_coords;
+    void main() {
+        // In order to return the texture coordinate for a specific
+        // vertex we have to know what vertex is currently being passed in.
+        // We do this through gl_VertexID which increments with every vertex passed in.
+        // We can figure out the rectangle specific index from the vertex id by modding it
+        // by 4. Example: if a vertex has id 16, then it is the first vertex of the fourth
+        // rectangle being drawn. 16 % 4 == 0 which correctly returns the first index.
+        if (gl_VertexID % 4 == 0) { // First vertex
+            v_tex_coords = vec2(0.0, 1.0);
+        } else if (gl_VertexID % 4 == 1) { // Second vertex
+            v_tex_coords = vec2(1.0, 1.0);
+        } else if (gl_VertexID % 4 == 2) { // Third vertex
+            v_tex_coords = vec2(0.0, 0.0);
+        } else { // Fourth vertex
+            v_tex_coords = vec2(1.0, 0.0);
+        }
+        gl_Position = projection * vec4(position, 0.0, 1.0);
+    }
+    "#;
+
+const FRAGMENT_SHADER_SRC: &str = r#"
+        #version 140
+    // Input texture coordinates passed from the vertex shader.
+    in vec2 v_tex_coords;
+    // Outputs the color for the specific fragment.
+    out vec4 color;
+    // Uniform parameter passed in from the frame.draw() call.
+    uniform sampler2D tex;
+    void main() {
+        // Applies a texture to the rectangle.
+        color = texture(tex, v_tex_coords);
+    }
+    "#;
 
 pub fn launch_window(){
     let events_loop = glutin::event_loop::EventLoop::new();
 
     let wb = glutin::window::WindowBuilder::new().with_inner_size(
-        glium::glutin::dpi::LogicalSize::new(1024.0, 768.0)
+        glium::glutin::dpi::LogicalSize::new(SCREEN_WIDTH, SCREEN_HEIGHT)
     ).with_title("DSS - Chilton").with_resizable(false);
 
     let cb = glutin::ContextBuilder::new();
@@ -85,30 +154,42 @@ pub fn launch_window(){
 
     let image = image::open("derp2.png").unwrap().to_rgb8();
     let dim = image.dimensions();
-    let img = glium::texture::RawImage2d::from_raw_rgb_reversed(&image.into_raw(), dim);
+    let img = glium::texture::RawImage2d::from_raw_rgb(image.into_raw(), dim);
     let tex: glium::texture::Texture2d = glium::texture::Texture2d::new(&display, img).unwrap();
-    let rect = Rectangle{ corners: [-0.5, -0.5, 0.5, 0.5], texture_key: "asdf" };
+    let mut rect = Rectangle{ center: [250., 250.], size: [200., 100.], texture_key: "asdf" };
     let mut hm = HashMap::new();
     hm.insert("asdf", tex);
 
-    let vertex_shader_src = r#"
-        #version 140
-        in vec2 position;
-        void main() {
-            gl_Position = vec4(position, 0.0, 1.0);
-        }
-    "#;
-
-    let fragment_shader_src = r#"
-        #version 140
-        out vec4 color;
-        void main() {
-            color = vec4(1.0, 0.0, 0.0, 1.0);
-        }
-    "#;
 
 
-    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+    let program = glium::Program::from_source(&display, VERTEX_SHADER_SRC, FRAGMENT_SHADER_SRC, None).unwrap();
+
+
+    let perspective = {
+        let matrix: Matrix4<f32> = cgmath::ortho(
+            0.0,
+            SCREEN_WIDTH as f32,
+            SCREEN_HEIGHT as f32,
+            0.0,
+            -1.0,
+            1.0
+        );
+        Into::<[[f32; 4]; 4]>::into(matrix)
+    };
+
+    // 0      1
+    // +------+
+    // |    / |
+    // |  /   |
+    // |/     |
+    // +------+
+    // 2      3
+    let ib_data: Vec<u16> = vec![0, 1, 2, 1, 3, 2];
+    let indices = glium::IndexBuffer::new(
+        &display,
+        glium::index::PrimitiveType::TrianglesList,
+        &ib_data
+    ).unwrap();
 
     let mut debounce = false;
 
@@ -116,9 +197,8 @@ pub fn launch_window(){
     events_loop.run(move |ev, _, control_flow|{
         let mut target = display.draw();
         target.clear_color(0.01, 0.01, 0.01, 1.0);
-        // target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
         // for r in &mut rv{
-        rect.draw(&mut target, &display, &program, &hm);
+        rect.draw(&mut target, &indices, &display, &program, &hm, perspective);
         // }
         target.finish().unwrap();
 
@@ -135,6 +215,8 @@ pub fn launch_window(){
                     let input = keyboard_input(&event, &mut debounce);
                     if input != (0.,0.) {
                         println!("{} {}", input.0, input.1);
+                        rect.center[0] += input.0;
+                        rect.center[1] += input.1;
                     }
                 }
 
